3991aca064806cc5f7cbe955266711841fe2a1d3
Karthik Viswanathan <me@karthikv.net>
2013-03-23 18:42:39 -0700

    Factor out constants. Add further documentation.

--- my-style.js
+++ my-style.js
@@ -1,56 +1,62 @@
-window.addEventListener('DOMContentLoaded', function(event) {
-  var head = document.getElementsByTagName('head')[0];
-  var body = document.body;
+// asynchronous self-invoking function to not pollute global namespace
+(function(window, document, undefined) {
+  var TAB_KEY_CODE = 9;
+  var M_KEY_CODE = 77;
 
-  var style = document.createElement('style');
-  var textarea = document.createElement('textarea');
+  var SOFT_TAB = '    ';
+  var SOFT_TAB_LENGTH = SOFT_TAB.length;
 
-  style.id = 'my-style';
-  textarea.id = 'my-style-input';
+  window.addEventListener('DOMContentLoaded', function(event) {
+    var head = document.getElementsByTagName('head')[0];
+    var body = document.body;
 
-  // hide textarea by default
-  textarea.style.display = 'none';
+    var style = document.createElement('style');
+    var textarea = document.createElement('textarea');
 
-  head.appendChild(style);
-  body.appendChild(textarea);
+    // hide textarea by default
+    textarea.style.display = 'none';
+    textarea.id = 'my-style-input';
 
-  style.innerHTML = localStorage.myStyle || '';
-  textarea.value = style.innerHTML;
+    head.appendChild(style);
+    body.appendChild(textarea);
 
-  // continually update styles with textarea content
-  textarea.addEventListener('keyup', function(event) {
-    style.innerHTML = textarea.value;
-    localStorage.myStyle = style.innerHTML;
-  });
+    style.innerHTML = localStorage.myStyle || '';
+    textarea.value = style.innerHTML;
 
-  textarea.addEventListener('keydown', function(event) {
-    var value = textarea.value;
-    var caret = textarea.selectionStart;
+    // continually update styles with textarea content
+    textarea.addEventListener('keyup', function(event) {
+      style.innerHTML = textarea.value;
+      localStorage.myStyle = style.innerHTML;
+    });
 
-    var softTab = '    ';
-    var softTabLength = softTab.length;
+    // pressing tab should insert spaces instead of focusing another element
+    textarea.addEventListener('keydown', function(event) {
+      var value = textarea.value;
+      var caret = textarea.selectionStart;
 
-    // if tab is pressed, insert four spaces
-    if (event.keyCode === 9) {
-      textarea.value = value.substring(0, caret) + softTab +
-        value.substring(caret);
+      // if tab is pressed, insert four spaces
+      if (event.keyCode === TAB_KEY_CODE) {
+        textarea.value = value.substring(0, caret) + SOFT_TAB +
+          value.substring(caret);
 
-      // move caret to after soft tab
-      textarea.setSelectionRange(caret + softTabLength, caret + softTabLength);
+        // move caret to after soft tab
+        textarea.setSelectionRange(caret + SOFT_TAB_LENGTH, caret +
+          SOFT_TAB_LENGTH);
 
-      // prevent default tab action that shifts focus to the next element
-      event.preventDefault();
-    }
-  });
-
-  window.addEventListener('keydown', function(event) {
-    // command + control + m toggles text area
-    if (event.metaKey && event.ctrlKey && event.keyCode === 77) {
-      if (textarea.style.display == 'none') {
-        textarea.style.display = 'block';
-      } else {
-        textarea.style.display = 'none';
+        // prevent default tab action that shifts focus to the next element
+        event.preventDefault();
       }
-    }
+    });
+
+    window.addEventListener('keydown', function(event) {
+      // command + control + m toggles text area
+      if (event.metaKey && event.ctrlKey && event.keyCode === M_KEY_CODE) {
+        if (textarea.style.display == 'none') {
+          textarea.style.display = 'block';
+        } else {
+          textarea.style.display = 'none';
+        }
+      }
+    });
   });
-});
+})(this, this.document);
